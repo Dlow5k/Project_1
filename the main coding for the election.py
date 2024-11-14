826e30ce12e11f0e24ee4d290a331fc4dce7a0ab
Dlow5k <david.jalogho2024@my.ntu.ac.uk>
2024-11-14 02:02:28 +0000

    Update the main coding for the election.py

--- the main coding for the election.py
+++ the main coding for the election.py
@@ -1,11 +1,10 @@
-Options = ["List the Constituency" , "List the region", "List the Constuency type", "List the valid votes"]
+Options = ["List the Constituency" , "List the region", "List the Constuency type", "List the valid votes","Vote"]
 
 print("Welcome to the Election, it's great to have you here")
 print("Choose your option(please intput a number)")
 print("Option Number\tOption")
 optionNumber = 0
 for option in Options:
-    print(optionNumber,"\t,option")
+    print(optionNumber,"\t\t",option)
     optionNumber += 1
-int(input("what is your option"))
-
+int(input("What is your option?"))
